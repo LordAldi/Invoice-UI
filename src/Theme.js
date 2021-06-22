490a71f8ffa8159069ceebfac3fd0068bf957847
LordAldi <aldianugra09@gmail.com>
2021-06-22 21:12:28 +0700

    add theme

--- src/Theme.js
+++ src/Theme.js
@@ -1,10 +1,40 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme, CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 
 const Theme = ({ children }) => {
-  const theme = createMuiTheme({});
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "rgba(130, 109, 255, 1)",
+      },
+      secondary: {
+        main: "rgba(236, 106, 44, 1)",
+      },
+      background: {
+        default: "rgba(248, 247, 252, 1)",
+        paper: "rgba(252, 252, 252, 1)",
+      },
+    },
+    overrides: {
+      MuiButton: {
+        root: {
+          borderRadius: "100px",
+        },
+      },
+    },
+    props: {
+      MuiButtonBase: {
+        disableRipple: true,
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Theme;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
+import { Box, Button } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>home <Link to="/login">Login</Link></div>;
+  return (
+    <Box>
+      home{" "}
+      <Link to="/login">
+        <Button color="primary" variant="contained">
+          Mark as paid
+        </Button>
+      </Link>
+      <Button color="secondary" variant="contained">
+        Delete
+      </Button>
+      <Button color="primary" variant="outlined">
+        Edit
+      </Button>
+    </Box>
+  );
 };
 
 export default Home;
